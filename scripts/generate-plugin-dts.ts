08f6f2446ac4d1a9e03096006368579319ee1d67
Shinigami92 <chrissi92@hotmail.de>
2023-10-18 18:12:21 +0200

    Setup project basics

--- scripts/generate-plugin-dts.ts
+++ scripts/generate-plugin-dts.ts
@@ -1,47 +1,63 @@
-import { pascalCase } from "change-case";
-import type { JSONSchema4 } from "json-schema";
-import { compile } from "json-schema-to-typescript";
-import { mkdir, readdir, writeFile } from "node:fs/promises";
-import { join } from "node:path";
-import { URL, fileURLToPath } from "node:url";
+import { pascalCase } from 'change-case';
+import type { ESLint, Rule } from 'eslint';
+import type { JSONSchema4 } from 'json-schema';
+import { compile } from 'json-schema-to-typescript';
+import { mkdir, readdir, writeFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { URL, fileURLToPath } from 'node:url';
+import type { IPackageJson } from 'package-json-type';
 
-const __dirname: string = fileURLToPath(new URL(".", import.meta.url));
+const __dirname: string = fileURLToPath(new URL('.', import.meta.url));
 
-const workspaceRootDir = join(__dirname, "..", "types");
+const workspaceRootDirectory = join(__dirname, '..', 'types');
 
-const workspaces = await readdir(workspaceRootDir, {
+const workspaces = await readdir(workspaceRootDirectory, {
   recursive: false,
 });
 
 for (const workspace of workspaces) {
-  const workspaceDir = join(workspaceRootDir, workspace);
+  const workspaceDirectory = join(workspaceRootDirectory, workspace);
 
-  const pkgJson = await import(join(workspaceDir, "package.json"));
-  const dep = Object.keys(pkgJson.devDependencies).find((dep) =>
-    dep.includes(workspace)
+  const packageJson: IPackageJson = await import(
+    join(workspaceDirectory, 'package.json')
+  );
+  const dep = Object.keys(packageJson.devDependencies ?? {}).find((dep) =>
+    dep.includes(workspace),
   );
 
   if (!dep) {
+    console.warn(`No matching dependency found for ${workspace}`);
     continue;
   }
 
   const pluginName = workspace;
 
-  const pluginDir = join(workspaceDir, "node_modules", dep);
-  const pluginPkgJson = await import(join(pluginDir, "package.json"));
-  const pluginEntry = pluginPkgJson.main;
-  const pluginModule = await import(join(pluginDir, pluginEntry));
+  const pluginDirectory = join(workspaceDirectory, 'node_modules', dep);
+  const pluginPackageJson: IPackageJson = await import(
+    join(pluginDirectory, 'package.json')
+  );
+  const pluginEntry = pluginPackageJson.main;
 
-  const pluginConfigs = Object.keys(pluginModule.configs);
+  if (!pluginEntry) {
+    console.warn(`No entry found for ${pluginName}`);
+    continue;
+  }
+
+  const pluginModule: ESLint.Plugin = await import(
+    join(pluginDirectory, pluginEntry)
+  );
+
+  const pluginConfigs = Object.keys(pluginModule.configs ?? {});
 
   const pluginRules = pluginModule.rules;
-  await mkdir(join(workspaceDir, "rules"), { recursive: true });
+  await mkdir(join(workspaceDirectory, 'rules'), { recursive: true });
 
   const ruleOptionImports: string[] = [];
   const ruleDeclarations: string[] = [];
 
-  for (const [ruleName, ruleDefinition] of Object.entries(pluginRules)) {
-    const meta = ruleDefinition.meta;
+  for (const [ruleName, ruleDefinition] of Object.entries(pluginRules ?? {})) {
+    // TODO @Shinigami92 2023-10-18: make this more TS safe
+    const meta = (ruleDefinition as Rule.RuleModule).meta!;
 
     let schemas = (meta.schema as JSONSchema4[]) ?? [];
     if (!Array.isArray(schemas)) {
@@ -51,51 +67,53 @@ for (const workspace of workspaces) {
     const options = await Promise.all(
       schemas.map(async (schema, index) => {
         schema = JSON.parse(
-          JSON.stringify(schema).replace(/\#\/items\/0\/\$defs\//g, "#/$defs/")
+          JSON.stringify(schema).replaceAll('#/items/0/$defs/', '#/$defs/'),
         );
 
         return await compile(schema, `Schema${index}`, {
-          bannerComment: "",
+          bannerComment: '',
         });
-      })
+      }),
     );
 
     await writeFile(
-      join(workspaceDir, "rules", `${ruleName}.d.ts`),
-      `${options.join("\n")}
+      join(workspaceDirectory, 'rules', `${ruleName}.d.ts`),
+      `${options.join('\n')}
 export type ${pascalCase(ruleName)}RuleOptions = [${options
         .map((_, index) => `Schema${index}?`)
-        .join(", ")}]
+        .join(', ')}]
 `,
       {
-        encoding: "utf-8",
-        flag: "w",
-      }
+        encoding: 'utf8',
+        flag: 'w',
+      },
     );
 
     ruleOptionImports.push(
       `import type { ${pascalCase(
-        ruleName
-      )}RuleOptions } from "./rules/${ruleName}"`
+        ruleName,
+      )}RuleOptions } from "./rules/${ruleName}"`,
     );
 
+    const documentation: Rule.RuleMetaData['docs'] = meta.docs ?? {};
+
     ruleDeclarations.push(`/**
-     * ${meta.docs.description}
+     * ${documentation.description}
      *
-     * @see [${ruleName}](${meta.docs.url})
+     * @see [${ruleName}](${documentation.url})
      */
     "${pluginName}/${ruleName}": ${pascalCase(ruleName)}RuleOptions;`);
   }
 
   await writeFile(
-    join(workspaceDir, "index.d.ts"),
-    `${ruleOptionImports.join("\n")}
+    join(workspaceDirectory, 'index.d.ts'),
+    `${ruleOptionImports.join('\n')}
 
 declare module "eslint-define-config" {
   export interface CustomExtends {
     ${pluginConfigs
       .map((config) => `"plugin:${pluginName}/${config}": void;`)
-      .join("\n")}
+      .join('\n')}
   }
 
   export interface CustomPlugins {
@@ -103,13 +121,13 @@ declare module "eslint-define-config" {
   }
 
   export interface CustomRuleOptions {
-    ${ruleDeclarations.join("\n")}
+    ${ruleDeclarations.join('\n')}
   }
 }
 `,
     {
-      encoding: "utf-8",
-      flag: "w",
-    }
+      encoding: 'utf8',
+      flag: 'w',
+    },
   );
 }
--- types/deprecation/rules/deprecation.d.ts
+++ types/deprecation/rules/deprecation.d.ts
@@ -1,2 +1,1 @@
-
-export type DeprecationRuleOptions = []
+export type DeprecationRuleOptions = [];
