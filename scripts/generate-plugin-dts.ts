1aa19cedce5e5fb51769d1da7254f4e9088f0539
Shinigami92 <chrissi92@hotmail.de>
2023-10-18 12:18:57 +0200

    fully dynamically generate deprecation

--- scripts/generate-plugin-dts.ts
+++ scripts/generate-plugin-dts.ts
@@ -1,4 +1,7 @@
-import { readdir, writeFile } from "node:fs/promises";
+import { pascalCase } from "change-case";
+import type { JSONSchema4 } from "json-schema";
+import { compile } from "json-schema-to-typescript";
+import { mkdir, readdir, writeFile } from "node:fs/promises";
 import { join } from "node:path";
 import { URL, fileURLToPath } from "node:url";
 
@@ -22,30 +25,85 @@ for (const workspace of workspaces) {
     continue;
   }
 
+  const pluginName = workspace;
+
   const pluginDir = join(workspaceDir, "node_modules", dep);
   const pluginPkgJson = await import(join(pluginDir, "package.json"));
   const pluginEntry = pluginPkgJson.main;
   const pluginModule = await import(join(pluginDir, pluginEntry));
-  console.log(pluginModule);
+
+  const pluginConfigs = Object.keys(pluginModule.configs);
+
+  const pluginRules = pluginModule.rules;
+  await mkdir(join(workspaceDir, "rules"), { recursive: true });
+
+  const ruleOptionImports: string[] = [];
+  const ruleDeclarations: string[] = [];
+
+  for (const [ruleName, ruleDefinition] of Object.entries(pluginRules)) {
+    const meta = ruleDefinition.meta;
+
+    let schemas = (meta.schema as JSONSchema4[]) ?? [];
+    if (!Array.isArray(schemas)) {
+      schemas = [schemas];
+    }
+
+    const options = await Promise.all(
+      schemas.map(async (schema, index) => {
+        schema = JSON.parse(
+          JSON.stringify(schema).replace(/\#\/items\/0\/\$defs\//g, "#/$defs/")
+        );
+
+        return await compile(schema, `Schema${index}`, {
+          bannerComment: "",
+        });
+      })
+    );
+
+    await writeFile(
+      join(workspaceDir, "rules", `${ruleName}.d.ts`),
+      `${options.join("\n")}
+export type ${pascalCase(ruleName)}RuleOptions = [${options
+        .map((_, index) => `Schema${index}?`)
+        .join(", ")}]
+`,
+      {
+        encoding: "utf-8",
+        flag: "w",
+      }
+    );
+
+    ruleOptionImports.push(
+      `import type { ${pascalCase(
+        ruleName
+      )}RuleOptions } from "./rules/${ruleName}"`
+    );
+
+    ruleDeclarations.push(`/**
+     * ${meta.docs.description}
+     *
+     * @see [${ruleName}](${meta.docs.url})
+     */
+    "${pluginName}/${ruleName}": ${pascalCase(ruleName)}RuleOptions;`);
+  }
 
   await writeFile(
     join(workspaceDir, "index.d.ts"),
-    `declare module "eslint-define-config" {
+    `${ruleOptionImports.join("\n")}
+
+declare module "eslint-define-config" {
   export interface CustomExtends {
-    "plugin:deprecation/recommended": void;
+    ${pluginConfigs
+      .map((config) => `"plugin:${pluginName}/${config}": void;`)
+      .join("\n")}
   }
 
   export interface CustomPlugins {
-    deprecation: void;
+    ${pluginName}: void;
   }
 
   export interface CustomRuleOptions {
-    /**
-     * Do not use deprecated APIs.
-     *
-     * @see [deprecation](https://github.com/gund/eslint-plugin-deprecation)
-     */
-    "deprecation/deprecation": [];
+    ${ruleDeclarations.join("\n")}
   }
 }
 `,
