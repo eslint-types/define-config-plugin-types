922248c54836ae96a3b56c5e1c616acd849e7490
Shinigami92 <chrissi92@hotmail.de>
2024-01-02 09:21:03 +0100

    Update packages

--- types/typescript-eslint/rules/switch-exhaustiveness-check.d.ts
+++ types/typescript-eslint/rules/switch-exhaustiveness-check.d.ts
@@ -1,5 +1,9 @@
 export interface Schema0 {
   /**
+   * If 'true', allow 'default' cases on switch statements with exhaustive cases.
+   */
+  allowDefaultCaseForExhaustiveSwitch?: boolean;
+  /**
    * If 'true', require a 'default' clause for switches on non-union types.
    */
   requireDefaultForNonUnion?: boolean;
