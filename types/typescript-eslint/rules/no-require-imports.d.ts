2c2ae3be59b31928e078de1199606e18d6acc957
Shinigami92 <chrissi92@hotmail.de>
2024-01-10 11:41:23 +0100

    Update packages

--- types/typescript-eslint/rules/no-require-imports.d.ts
+++ types/typescript-eslint/rules/no-require-imports.d.ts
@@ -1,1 +1,8 @@
-export type NoRequireImportsRuleOptions = [];
+export interface Schema0 {
+  /**
+   * Patterns of import paths to allow requiring from.
+   */
+  allow?: string[];
+}
+
+export type NoRequireImportsRuleOptions = [Schema0?];
--- types/typescript-eslint/rules/no-var-requires.d.ts
+++ types/typescript-eslint/rules/no-var-requires.d.ts
@@ -1,1 +1,8 @@
-export type NoVarRequiresRuleOptions = [];
+export interface Schema0 {
+  /**
+   * Patterns of import paths to allow requiring from.
+   */
+  allow?: string[];
+}
+
+export type NoVarRequiresRuleOptions = [Schema0?];
